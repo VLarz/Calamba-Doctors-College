75f382937d5cc76b83b08e36edc644707373e9dd
Vhen Larson A. Dela Cuesta <vhenlarsondelacuesta.1398@gmail.com>
2020-10-03 16:41:23 +0800

    added ngx-scroll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-
+import { NgxPageScrollModule } from 'ngx-page-scroll';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -13,6 +12,7 @@ import { AboutComponent } from './about/about-cdc/about.component';
 import { CollegeComponent } from './academics/college/college.component';
 import { RequirementsComponent } from './admissions/requirements/requirements.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { NgxPageScrollCoreModule } from 'ngx-page-scroll-core';
 
 
 @NgModule({
@@ -30,7 +30,9 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxPageScrollCoreModule.forRoot({duration: 300, scrollOffset: 50}),
+    NgxPageScrollModule
   ],
   providers: [],
   bootstrap: [AppComponent]
